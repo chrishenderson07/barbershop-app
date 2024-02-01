deb6b086817a104af6390eb58ce57f6ba835db40
chrishenderson07 <chris.hend012@gmail.com>
2024-02-01 08:59:25 -0300

    fix: layout footer

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -59,7 +59,6 @@ export default async function Home() {
 					))}
 				</div>
 			</div>
-			<Footer />
 		</>
 	)
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,10 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={`${inter.className} dark`}>{children}</body>
+			<body className={`${inter.className} dark`}>
+				{children}
+				<Footer />
+			</body>
 		</html>
 	)
 }
