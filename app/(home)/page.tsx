da43f43aa08ae7ee7f147b08cc0463fba79f58ff
chrishenderson07 <chris.hend012@gmail.com>
2024-02-01 08:57:43 -0300

    feat: add footer component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,6 +5,7 @@ import { Search } from './_components/search'
 import { BookingItem } from '../_components/booking-item'
 import { db } from '../_lib/prisma'
 import { BarbershopItem } from './_components/barbershop-item'
+import { Footer } from '../_components/footer'
 
 export default async function Home() {
 	// chamar prisma e pegar barbearias
@@ -43,6 +44,22 @@ export default async function Home() {
 					))}
 				</div>
 			</div>
+
+			<div className="mt-6 mb-[4.5rem]">
+				<h2 className="text-xs mb-3 uppercase text-gray-400 font-bold">
+					Populares
+				</h2>
+
+				<div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+					{barbershops.map((barbershop) => (
+						<BarbershopItem
+							key={barbershop.id}
+							barbershop={barbershop}
+						/>
+					))}
+				</div>
+			</div>
+			<Footer />
 		</>
 	)
 }
---
+++ app/_components/footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+	return (
+		<footer className="w-full bg-secondary py-6 px-5">
+			<p className="text-gray-400 text-xs font-bold opacity-75">
+				₢ 2023 Copyright FSW Barbershop
+			</p>
+		</footer>
+	)
+}
