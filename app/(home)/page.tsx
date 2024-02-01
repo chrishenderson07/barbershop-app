735bbbd67a271d84af489a264a325570b461c977
chrishenderson07 <chris.hend012@gmail.com>
2024-02-01 07:37:49 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,19 @@
+import { format } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
 import { Header } from '../_components/header'
 
 export default function Home() {
-	return <Header />
+	return (
+		<>
+			<Header />
+			<div className="px-5 pt-5">
+				<h2 className="text-xl font-bold">Ola, Fulano!</h2>
+				<p className="capitalize text-sm">
+					{format(new Date(), "EEEE',' dd 'de' MMMM", {
+						locale: ptBR,
+					})}
+				</p>
+			</div>
+		</>
+	)
 }
