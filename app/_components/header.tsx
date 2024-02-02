608a76fea5a7288789f695da5cc6d240af42dfd4
chrishenderson07 <chris.hend012@gmail.com>
2024-02-02 08:01:49 -0300

    feat: google auth works

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,9 +1,15 @@
+'use client'
+
 import Image from 'next/image'
 import { Card, CardContent } from './ui/card'
 import { Button } from './ui/button'
 import { MenuIcon } from 'lucide-react'
+import { signIn } from 'next-auth/react'
 
 export const Header = () => {
+	const handleLoginClick = async () => {
+		await signIn()
+	}
 	return (
 		<Card>
 			<CardContent className="px-5 py-8 flex flex-row items-center justify-between">
@@ -13,12 +19,13 @@ export const Header = () => {
 					height={22}
 					width={120}
 				/>
-				<Button
+				{/* <Button
 					variant="outline"
 					size="icon"
 					className="h-8 w-8">
 					<MenuIcon size={16} />
-				</Button>
+				</Button> */}
+				<Button onClick={handleLoginClick}>Login</Button>
 			</CardContent>
 		</Card>
 	)
