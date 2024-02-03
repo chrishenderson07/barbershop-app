d55f66eaa8dad2a9163eb1d2985854b53cb7fcbd
chrishenderson07 <chris.hend012@gmail.com>
2024-02-03 08:20:40 -0300

    feat: add side menu

--- app/barbershops/[id]/_components/barbershop-info.tsx
+++ app/barbershops/[id]/_components/barbershop-info.tsx
@@ -1,5 +1,7 @@
 'use client'
+import SideMenu from '@/app/_components/side-menu'
 import { Button } from '@/app/_components/ui/button'
+import { Sheet, SheetContent, SheetTrigger } from '@/app/_components/ui/sheet'
 import { Barbershop } from '@prisma/client'
 import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from 'lucide-react'
 import Image from 'next/image'
@@ -29,6 +31,21 @@ const BarbershopInfo = ({ barbershop }: BarbershopInfoProps) => {
 					className="z-50 top-4 right-4 absolute">
 					<MenuIcon />
 				</Button>
+
+				<Sheet>
+					<SheetTrigger asChild>
+						<Button
+							variant="outline"
+							size="icon">
+							<MenuIcon size={16} />
+						</Button>
+					</SheetTrigger>
+
+					<SheetContent className="p-0">
+						<SideMenu />
+					</SheetContent>
+				</Sheet>
+
 				<Image
 					src={barbershop.imageUrl}
 					alt={barbershop.name}
