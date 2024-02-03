6884891270429b782b967a58a6feee6db0519deb
chrishenderson07 <chris.hend012@gmail.com>
2024-02-03 07:33:36 -0300

    feat: add google Auth Work

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -4,9 +4,10 @@ import Image from 'next/image'
 import { Card, CardContent } from './ui/card'
 import { Button } from './ui/button'
 import { MenuIcon } from 'lucide-react'
-import { signIn } from 'next-auth/react'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 export const Header = () => {
+	const { data } = useSession()
 	const handleLoginClick = async () => {
 		await signIn()
 	}
@@ -19,13 +20,12 @@ export const Header = () => {
 					height={22}
 					width={120}
 				/>
-				{/* <Button
+				<Button
 					variant="outline"
 					size="icon"
 					className="h-8 w-8">
 					<MenuIcon size={16} />
-				</Button> */}
-				<Button onClick={handleLoginClick}>Login</Button>
+				</Button>
 			</CardContent>
 		</Card>
 	)
